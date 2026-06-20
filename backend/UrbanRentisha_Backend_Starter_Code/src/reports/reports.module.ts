cd93d65223adc141f01d282857d9fd7fec88840c
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-06-20 22:47:33 +0300

    feat: import NotificationsModule into reports module

--- backend/UrbanRentisha_Backend_Starter_Code/src/reports/reports.module.ts
+++ backend/UrbanRentisha_Backend_Starter_Code/src/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ReportsController } from "./reports.controller";
 import { ReportsService } from "./reports.service";
 import { AuditLogsModule } from "../audit-logs/audit-logs.module";
+import { NotificationsModule } from "../notifications/notifications.module";
 
 @Module({
-  imports: [AuditLogsModule],
+  imports: [AuditLogsModule, NotificationsModule],
   controllers: [ReportsController],
   providers: [ReportsService],
   exports: [ReportsService],
