59d9799921892162b6f31d361912593c78a85354
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-06-20 22:47:22 +0300

    feat: import NotificationsModule into payments module

--- backend/UrbanRentisha_Backend_Starter_Code/src/payments/payments.module.ts
+++ backend/UrbanRentisha_Backend_Starter_Code/src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsController } from "./payments.controller";
 import { PaymentsService } from "./payments.service";
 import { StellarModule } from "../stellar/stellar.module";
 import { AuditLogsModule } from "../audit-logs/audit-logs.module";
+import { NotificationsModule } from "../notifications/notifications.module";
 
 @Module({
-  imports: [StellarModule, AuditLogsModule],
+  imports: [StellarModule, AuditLogsModule, NotificationsModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
