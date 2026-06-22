86127fb3d43fdbc2847bf280a8f588b4100c4a35
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-06-22 17:20:04 +0300

    feat: register EscrowService as a provider of SorobanModule

--- backend/UrbanRentisha_Backend_Starter_Code/src/soroban/soroban.module.ts
+++ backend/UrbanRentisha_Backend_Starter_Code/src/soroban/soroban.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { SorobanService } from "./soroban.service";
+import { EscrowService } from "./escrow.service";
 
 @Module({
-  providers: [SorobanService],
-  exports: [SorobanService],
+  providers: [SorobanService, EscrowService],
+  exports: [SorobanService, EscrowService],
 })
 export class SorobanModule {}
