822a9e26b20a077a1ad34f7b7c28aaabd2626de3
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-06-23 04:53:55 +0300

    feat: wire RealtimeModule into MessagesModule

--- backend/UrbanRentisha_Backend_Starter_Code/src/messages/messages.module.ts
+++ backend/UrbanRentisha_Backend_Starter_Code/src/messages/messages.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { MessagesController } from "./messages.controller";
 import { MessagesService } from "./messages.service";
 import { NotificationsModule } from "../notifications/notifications.module";
+import { RealtimeModule } from "../realtime/realtime.module";
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, RealtimeModule],
   controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService],
